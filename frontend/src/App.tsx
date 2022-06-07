66582f43fd93e96a6ed68a1ce078ef02db993e46
jonnef <kennethbolland@online.de>
2022-06-07 16:21:53 +0200

    toolbar mockup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import * as Mui from '@mui/material/';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div>
+      <Mui.AppBar position="static">
+        <Mui.Toolbar>
+        </Mui.Toolbar>
+      </Mui.AppBar> 
     </div>
   );
 }
