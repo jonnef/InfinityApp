6b6e7cb9cee111bd1b3b2e393cfb8fd6badeb589
kenneth.bolland <kenneth.bolland@telekom.de>
2022-07-12 22:32:32 +0200

    added AccountButton to AppBar

--- frontend/src/comps/InfinityAppBar.tsx
+++ frontend/src/comps/InfinityAppBar.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
+// import React from 'react';
 import * as Mui from '@mui/material';
 import MenuButton from './Button/MenuButton';
 import { ThemeProvider } from '@mui/material/styles';
+import AccountButton from './Button/AccountButton';
 import defaultTheme from './Theme';
 
-function InfinityAppBar(){
+function InfinityAppBar(props: {name: String}){
   return(
     <div className="IAppBar">
       <ThemeProvider theme={defaultTheme}>
       	<Mui.AppBar position="static" color="primary">
-        	<Mui.Toolbar>
+        	<Mui.Toolbar style={{justifyContent: "space-between"}}>
           	<MenuButton />
+            <Mui.Typography variant="h4">
+              {props.name}
+            </Mui.Typography>
+            <AccountButton />
       		</Mui.Toolbar>
       	</Mui.AppBar>
       </ThemeProvider>
