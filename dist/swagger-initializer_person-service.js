0793f1938a8e80741b5e96a975ee46573c67fe25
jonnef <kennethbolland@online.de>
2022-07-18 17:10:39 +0200

    added folder for services, changed api dir, modified index-html

--- dist/swagger-initializer_person-service.js
+++ dist/swagger-initializer_person-service.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "./person-service/openapi.yaml",
+    url: "./person-service/api/person-service.openapi.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
